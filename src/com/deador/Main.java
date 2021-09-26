6cdead0db301e6d7f73aa5077798a59d4f59a1d4
Deador <inkohnito4@gmail.com>
2021-09-26 16:36:58 +0300

    Update 0.1

--- src/com/deador/Main.java
+++ src/com/deador/Main.java
@@ -24,28 +24,29 @@ public class Main {
         }
         System.out.println();
         */
-        System.out.println("Enter int number for ArrayList: ");
-        ArrayList<Integer> secondArrayList = new ArrayList<Integer>();
-        for (int i = 0; i < 4; i++) {
-            while (!scanner.hasNextInt()) {
-                scanner.next();
-                System.out.println("Error. Enter int number.");
-            }
-            secondArrayList.add(scanner.nextInt());
-        }
-        for (int i = 0; i < secondArrayList.size(); i++) {
-            System.out.print(secondArrayList.get(secondArrayList.size() - i - 1) + " ");
-        }
-        System.out.println();
-        System.out.println("Removing the last 2 numbers of ArrayList.");
-        secondArrayList.remove(secondArrayList.size() - 1);
-        secondArrayList.remove(secondArrayList.size() - 1);
-        System.out.println("New ArrayList: ");
-        for (Integer integer : secondArrayList) {
-            System.out.print(integer + " ");
-        }
-        System.out.println("Hello world");
-        System.out.println();
+        //------------------------------------------------------------------------------------------------------------------------------------------
+//        System.out.println("Enter int number for ArrayList: ");
+//        ArrayList<Integer> secondArrayList = new ArrayList<Integer>();
+//        for (int i = 0; i < 4; i++) {
+//            while (!scanner.hasNextInt()) {
+//                scanner.next();
+//                System.out.println("Error. Enter int number.");
+//            }
+//            secondArrayList.add(scanner.nextInt());
+//        }
+//        for (int i = 0; i < secondArrayList.size(); i++) {
+//            System.out.print(secondArrayList.get(secondArrayList.size() - i - 1) + " ");
+//        }
+//        System.out.println();
+//        System.out.println("Removing the last 2 numbers of ArrayList.");
+//        secondArrayList.remove(secondArrayList.size() - 1);
+//        secondArrayList.remove(secondArrayList.size() - 1);
+//        System.out.println("New ArrayList: ");
+//        for (Integer integer : secondArrayList) {
+//            System.out.print(integer + " ");
+//        }
+//        System.out.println("Hello world");
+//        ------------------------------------------------------------------------------------------------------------------------------------------
 
     }
 }
